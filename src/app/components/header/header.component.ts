a658e4153f7d7030b6c2270c0584ff8d692ac3af
mvpclash <roy3r91@gmail.com>
2024-07-24 15:43:09 -0600

    Se configuró menú responsivo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,19 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  isMenuOpen = false;
-  public urls : string[] = [
-    'Inicio',
-    'Transparencia',
-    'Cafta',
-    'Contacto'
-  ]
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-  constructor() { }
+  // isMenuOpen = false;
+  // public urls : string[] = [
+  //   'Inicio',
+  //   'Transparencia',
+  //   'Cafta',
+  //   'Contacto'
+  // ]
+  // toggleMenu(): void {
+  //   this.isMenuOpen = !this.isMenuOpen;
+  // }
+  // constructor() { }
+  // menu_icon_variable: boolean = false;
+  // menuVariable: boolean = false;
 
   ngOnInit(): void {
+  }
+  // openMenu() {
+  //   this.menuVariable =! this.menuVariable;
+  //   this.menu_icon_variable =! this.menu_icon_variable;
+  // }
+
+  menuValue:boolean=false;
+ menu_icon :string ='bi bi-list';
+ openMenu(){
+    this.menuValue =! this.menuValue ;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+   closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
   }
 
 }
